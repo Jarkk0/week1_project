dc1aef567492ad923deaa6c85d0fcdb2407cd5ff
Jarkko <uusijarkko@gmail.com>
2021-09-24 14:05:47 +0300

    t2 task 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+window.appendList = function appendList() {
+  var ul = document.getElementById("listitems");
+  var li = document.createElement("li");
+  var item = document.getElementById("item");
+  li.appendChild(document.createTextNode(item.value));
+  ul.appendChild(li);
+};
+
+window.deleteList = function deleteList() {
+  var ul = document.getElementById("listitems");
+  while (ul.firstChild) ul.removeChild(ul.firstChild);
+};
